7f079d4f87eb7585a84d491b30ce3502de122928
Matt Felton <matthew.felton@senecacollege.ca>
2021-03-27 17:58:52 -0400

    added tailwind css header and footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, {useState} from 'react';
 import NameForm from './NameForm';
 import Thanks from './Thanks';
 import About from './About';
-import Topics from './Topics';
+import Legal from './Legal';
+import Footer from './Footer';
 
 import './App.css';
 import {
@@ -24,30 +25,46 @@ function App() {
   return (
     <Router>
     <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+      <header class="container-fluid bg-gray-800">
+        <div class="container mx-auto bg-gray-800 p-5">
+          <nav class="flex flex-row md:justify-between">
+      <Link to ="/"><img src="logo.png" alt=""></img></Link>
+        <ul class="flex text-white pt-8">
+          <li class="pr-5 hover:text-red-600">
+            <Link to="/">Contest</Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li class="pr-5 hover:text-red-600">
+            <Link to="/about">Product</Link>
           </li>
-          <li>
-            <Link to="/topics">Topics</Link>
+          <li class="pr-5 hover:text-red-600">
+            <Link to="/topics">Contact</Link>
+          </li>
+          <li class="pr-5 hover:text-red-600">
+            <Link to="/legal">Legal</Link>
           </li>
         </ul>
       </nav>
+      </div>
+     
+      </header>
+      
+  
       <h1>Here we go again</h1>
 
+     
+     
     <Switch>
       <Route exact path="/" children={<NameForm handleChange={(e)=>handleChange(e)} />} />
       <Route path="/thanks" children={<Thanks namer={namer} />} />
       <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <Route path="/legal" component={Legal} />
       
     </Switch>
+    <Footer />
     </div>
     </Router>
+ 
+  
   
   );
 }
---
+++ src/Legal.js
@@ -0,0 +1,10 @@
+function Legal() {
+    return (
+        <div>
+        <h1>Legal</h1>
+        <p>Legal page</p>
+        </div>
+    )
+}
+
+export default Legal;
