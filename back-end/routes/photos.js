35875f7ff1d8090f8abb77079a25c6159d226c9d
Matt Felton <matthew.felton@senecacollege.ca>
2021-04-07 19:01:17 -0400

    successfully connected mongodb

---
+++ back-end/routes/photos.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+
+
+//getting all
+router.get('/', (req, res) => {
+
+})
+
+//getting one
+router.get('/:id', (req, res) => {
+   res.send('Hello World')
+})
+
+//creating one
+router.post('/', (req, res) => {
+   
+})
+
+//updating one
+router.patch('/:id', (req, res) => {
+   
+})
+//deleting one
+router.delete('/:id', (req, res) => {
+   
+})
+
+
+module.exports = router
